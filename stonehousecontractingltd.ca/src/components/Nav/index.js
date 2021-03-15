0e97e57d04f2a27717d47eb2715458fc1597b900
Richard Antao <richardmantao@gmail.com>
2021-03-15 00:41:38 -0400

    Updated nac link order

--- stonehousecontractingltd.ca/src/components/Nav/index.js
+++ stonehousecontractingltd.ca/src/components/Nav/index.js
@@ -2,16 +2,18 @@ import React from "react";
 
 import "./styles.scss";
 
+import logo from "images/icon.png"
+
 const Nav = () =>
 	<nav id="nav" role="navigation">
 		<a href="/">
-			<img src="" />
+			<img id="logo" src={logo} alt="Stone House Contracting logo" />
 		</a>
 		<div className="nav-links">
 			<a href="/">Home</a>
-			<a href="/about">About</a>
 			<a href="/services">Services</a>
 			<a href="/gallery">Gallery</a>
+			<a href="/about">About</a>
 			<a href="/contact">Contact</a>
 		</div>
 	</nav>;
