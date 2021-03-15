02972f5bcffbd779c02c0c04a3d7e2e4a30f42ed
Richard Antao <richardmantao@gmail.com>
2021-03-15 14:45:53 -0400

    Lint

--- stonehousecontractingltd.ca/src/components/Footer/index.js
+++ stonehousecontractingltd.ca/src/components/Footer/index.js
@@ -2,16 +2,20 @@ import React from "react";
 
 import "./styles.scss";
 
-const year = new Date().getFullYear();
-
 const Footer = () =>
 	<footer id="footer" role="contentinfo">
 		<div id="footer-content-wrapper">
 			<p>
-				© Stone House Contracting {year}
+				© Stone House Contracting Ltd.
 			</p>
 			<p>
-				Designed and developed by <a href="https://richardantao.com" target="_blank" rel="noopener noreferrer">Richard Antao</a>
+				Designed and developed by <a
+					href="https://richardantao.com"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Richard Antao
+				</a>
 			</p>
 		</div>
 	</footer>;
