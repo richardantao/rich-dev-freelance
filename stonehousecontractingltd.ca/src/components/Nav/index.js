75100136943d176ae401e9505a5b732cce834e38
Richard Antao <richardmantao@gmail.com>
2021-03-17 14:39:14 -0400

    Updated logo

--- stonehousecontractingltd.ca/src/components/Nav/index.js
+++ stonehousecontractingltd.ca/src/components/Nav/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./styles.scss";
 
-import logo from "images/icon.png"
+import logo from "images/logo.png"
 
 const Nav = () =>
 	<nav id="nav" role="navigation">
